a03b4c29e169b9868b371247b420aff91ad225cd
Manoj <kommumanoj134@gmail.com>
2026-03-17 22:24:12 +0530

    updated AI interview bot

--- app.py
+++ app.py
@@ -216,5 +216,8 @@ def evaluate():
 # -----------------------------
 # RUN
 # -----------------------------
-if __name__=="__main__":
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Render gives PORT
+    app.run(host="0.0.0.0", port=port)
